3402a65f37ea6976569438906fc5d87c0b903493
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-23 22:32:34 +0100

    let's go! :tada:

---
+++ src/pages/index.tsx
@@ -0,0 +1,32 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+const Home: NextPage = () => {
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Norri</title>
+                <meta name="description" content="Norri" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main className={styles.main}>
+                <h1 className={styles.title}>Welcome to <span className={styles.rainbow}>Norri!</span></h1>
+                <p className={styles.description}>Coming soon...</p>
+            </main>
+
+            <footer className={styles.footer}>
+                <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">
+                    Powered by{' '}
+                    <span className={styles.logo}>
+            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          </span>
+                </a>
+            </footer>
+        </div>
+    );
+}
+
+export default Home;
